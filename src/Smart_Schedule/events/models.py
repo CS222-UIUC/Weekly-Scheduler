2d4a1b1fbc963e2f64f150ef6ffbbfa2d68e2fc5
Zavaonah <rubenzneyroud@gmail.com>
2022-10-21 13:52:51 -0500

    Added submit form and started on handling submissions

--- src/Smart_Schedule/events/models.py
+++ src/Smart_Schedule/events/models.py
@@ -1,9 +1,14 @@
+from email.policy import default
+from enum import auto
 from django.db import models
+from datetime import date
 
 class Event(models.Model):
     title=models.CharField(max_length=150)
-    duration=models.IntegerField()
+    duration=models.DurationField()
     block=models.BooleanField(default=False)
     def __str__(self):
         #it will return the title
         return self.title
+
+
--- weekly-calendar-react/src/App.js
+++ weekly-calendar-react/src/App.js
@@ -1,18 +1,96 @@
 
 import './App.css';
-import TestComponent from './components/TestComponet';
+//import Modal from './components/Modal';
 import CalendarComponent from './components/CalendarComponent';
+import { render } from 'react-dom';
+import React, { Component } from "react";
+import Modal from "./components/Modal";
+import axios from 'axios'; 
 
-
-function App() {
-  return (
-    <div className="App">
-      <CalendarComponent></CalendarComponent>
-      <TestComponent displaytext = "app test"/>
-      <button type="submit">
-        Test Button</button>
-    </div>
-  );
+class App extends Component {
+ 
+  // add a constructor to take props
+  constructor(props) {
+    super(props);
+     
+    // add the props here
+    this.state = {
+     
+      // the viewCompleted prop represents the status
+      // of the task. Set it to false by default
+      activeItem: {
+        title: "",
+        duration: "",
+        block: false
+      },
+       
+    };
+    //taskList[];
+  }
+ 
+  // Add componentDidMount()
+  componentDidMount() {
+    this.refreshList();
+  }
+ 
+  
+  refreshList = () => {
+    axios   //Axios to send and receive HTTP requests
+      .get("http://localhost:8000/api/tasks/")
+      .then(res => this.setState({ taskList: res.data }))
+      .catch(err => console.log(err));
+  };
+ 
+  toggle = () => {
+    //add this after modal creation
+    this.setState({ modal: !this.state.modal });
+  };
+  handleSubmit = (item) => {
+    this.toggle();
+    alert("save" + JSON.stringify(item));
+  };
+ 
+  // Submit an item
+  handleSubmit = (item) => {
+    this.toggle();
+    if (item.id) {
+      // if old post to edit and submit
+      axios
+        .put(`http://localhost:8000/api/tasks/${item.id}/`, item)
+        .then((res) => this.refreshList());
+      return;
+    }
+    // if new post to submit
+    axios
+      .post("http://localhost:8000/api/tasks/", item)
+      .then((res) => this.refreshList());
+  };
+ 
+  // Create item
+  createItem = () => {
+    const item = { title: "", duration: "", block: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+  // Start by visual effects to viewer
+  render() {
+    return (
+      <main className="content">
+        <CalendarComponent></CalendarComponent>
+        <h1 className="text-success text-uppercase text-center my-4">
+          Weekly Calendar
+        </h1>
+        <button onClick={this.createItem} className="btn btn-info">
+        Add task
+        </button>
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ) : null}
+      </main>
+    );
+  }
 }
-
 export default App;
