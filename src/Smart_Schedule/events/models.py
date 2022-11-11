37b8f0e67ec62816623b4916e518a05cae853dbb
Zavaonah <rubenzneyroud@gmail.com>
2022-11-10 19:46:49 -0600

    Created finalized events with start and end time

--- src/Smart_Schedule/events/models.py
+++ src/Smart_Schedule/events/models.py
@@ -12,5 +12,18 @@ class Event(models.Model):
         #it will return the titlepython
         return self.title
 
+class FinalManager(models.Manager):
+    def create_Finalized(self, title, duration, start, day):
+        finalized = self.create(title=title, start=start, end=start+duration, day=day)
+        return finalized
+class Finalized(models.Model):
+    title=models.CharField(max_length=150)
+    start=models.DateTimeField()
+    end=models.DateTimeField()
+    day=models.CharField(max_length=150)
+    objects = FinalManager()
+    def __str__(self):
+        #it will return the titlepython
+        return self.title
 
 
--- src/Smart_Schedule/events/views.py
+++ src/Smart_Schedule/events/views.py
@@ -10,6 +10,8 @@ from .serializers import EventSerializer
  
 # import the Event model from the models file
 from .models import Event
+from .models import Finalized
+from .models import FinalManager
  
 # create a class for the Event model viewsets
 class EventView(viewsets.ModelViewSet):
@@ -23,7 +25,20 @@ class EventView(viewsets.ModelViewSet):
     queryset = Event.objects.all()
 
 def sort_events(request):
-    #mon = dt.datetime.strptime('8:00:00 AM','%I:%M:%S %p')
-    monday = Event.objects.filter(day="monday").order_by('duration')
-    return render(request, 'event_list.html', {'monday' : monday })
+    Finalized.objects.all().delete()
+    mon_start = dt.datetime.strptime('8:00:00 AM','%I:%M:%S %p')
+    tues_start = dt.datetime.strptime('8:00:00 AM','%I:%M:%S %p')
+    mon = Event.objects.filter(day="monday").order_by('duration')
+    for i in mon:
+        Finalized.objects.create_Finalized(i.title, i.duration, mon_start, i.day)
+        mon_start = mon_start + i.duration
+    
+    monday = Finalized.objects.filter(day="monday").order_by('-start')
+
+    tues = Event.objects.filter(day="tuesday").order_by('duration')
+    for i in tues:
+        Finalized.objects.create_Finalized(i.title, i.duration, tues_start, i.day)
+        tues_start += i.duration
+    tuesday = Finalized.objects.filter(day="tuesday").order_by('-start')
+    return render(request, 'event_list.html', {'monday' : monday, 'tuesday' : tuesday })
 
