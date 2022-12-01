75d168b4ba7649b54cf6cc0997512526e138ea26
alntao <alantt2@illinois.edu>
2022-12-01 00:33:03 -0600

    test event can now be displayed

--- weekly-calendar-react/src/App.js
+++ weekly-calendar-react/src/App.js
@@ -10,83 +10,14 @@ import axios from 'axios';
 class App extends Component {
  
   // add a constructor to take props
-  constructor(props) {
-    super(props);
-     
-    // add the props here
-    this.state = {
-     
-      activeItem: {
-        title: "",
-        duration: "",
-        block: false
-      },
-       taskList: []
-    };
-    
-  }
- 
-  // Add componentDidMount()
-  componentDidMount() {
-    this.refreshList();
-  }
- 
   
-  refreshList = () => {
-    axios   //Axios to send and receive HTTP requests
-      .get("http://localhost:8000/api/tasks/")
-      .then(res => this.setState({ taskList: res.data }))
-      .catch(err => console.log(err));
-  };
- 
-  toggle = () => {
-    //add this after modal creation
-    this.setState({ modal: !this.state.modal });
-  };
-  handleSubmit = (item) => {
-    this.toggle();
-    alert("save" + JSON.stringify(item));
-  };
- 
-  // Submit an item
-  handleSubmit = (item) => {
-    this.toggle();
-    if (item.id) {
-      // if old post to edit and submit
-      axios
-        .put(`http://localhost:8000/api/tasks/${item.id}/`, item)
-        .then((res) => this.refreshList());
-      return;
-    }
-    // if new post to submit
-    axios
-      .post("http://localhost:8000/api/tasks/", item)
-      .then((res) => this.refreshList());
-  };
- 
-  // Create item
-  createItem = () => {
-    const item = { title: "", duration: "",day: "", block: false };
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  };
   // Start by visual effects to viewer
   render() {
     return (
       <main className="content">
         <CalendarComponent></CalendarComponent>
-        <h1 className="text-success text-uppercase text-center my-4">
-          Weekly Calendar
-        </h1>
-        <button onClick={this.createItem} className="btn btn-info">
-        Add task
-        </button>
-        {this.state.modal ? (
-          <Modal
-            activeItem={this.state.activeItem}
-            toggle={this.toggle}
-            onSave={this.handleSubmit}
-          />
-        ) : null}
+        
+      
       </main>
     );
   }
--- weekly-calendar-react/src/components/CalendarComponent.js
+++ weekly-calendar-react/src/components/CalendarComponent.js
@@ -1,34 +1,143 @@
 import React, {Component} from 'react';
-import {DayPilot, DayPilotCalendar} from "@daypilot/daypilot-lite-react";
-//import "./CalendarStyles.css";
+import {DayPilot, DayPilotCalendar, DayPilotNavigator} from "@daypilot/daypilot-lite-react";
+import Modal from "./Modal";
+import axios from 'axios'; 
+
+const styles = {
+  wrap: {
+    display: "flex"
+  },
+  left: {
+    marginRight: "10px"
+  },
+  main: {
+    flexGrow: "1"
+  }
+};
+
+
+
 class Calendar extends Component {
 
   constructor(props) {
     super(props);
+    this.calendarRef = React.createRef();
     this.state = {
-      viewType: "Week",
-      durationBarVisible: true,
-      
+      viewType: "WorkWeek",
+      durationBarVisible: false,
+      businessBeginsHour: 8,
+          businessEndsHour: 20,
+
+         
+      heightSpec: "BusinessHours",
+     
+      activeItem: {
+        title: "",
+        duration: "",
+        block: false
+      },
+       taskList: [],
+    
+    
+    
     };
   }
 
+   refreshList = () => {
+    axios   //Axios to send and receive HTTP requests
+      .get("http://localhost:8000/api/tasks/")
+      .then(res => this.setState({ taskList: res.data }))
+      .catch(err => console.log(err));
+  };
 
+  toggle = () => {
+    //add this after modal creation
+    this.setState({ modal: !this.state.modal });
+  };
+ 
+  // Submit an item
+  handleSubmit = (item) => {
+    this.toggle();
+    if (item.id) {
+      // if old post to edit and submit
+      axios
+        .put(`http://localhost:8000/api/tasks/${item.id}/`, item)
+        .then((res) => this.refreshList());
+      return;
+    }
+    // if new post to submit
+    axios
+      .post("http://localhost:8000/api/tasks/", item)
+      .then((res) => this.refreshList());
+  };
+ 
+  // Create item
+  createItem = () => {
+    const item = { title: "", duration: "",day: "", block: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
 
+  get calendar() {
+    return this.calendarRef.current.control;
+  }
 
+  componentDidUpdate() {
+    this.refreshList();
+    const item = this.activeItem;
+    const events = [
+      {
+        id: 1,
+        text: "this.state.taskList[0]",
+        start: "2022-11-28T08:00:00",
+        end: "2022-11-28T10:00:00"
+      },
+      
+    ];
+
+    const startDate = DayPilot.Date.today();
+
+    this.calendar.update({startDate, events});
+
+  }
+
+  // rendertasks = () => {
+    
+  // }
 
   render() {
-    const {...config} = this.state;
     return (
-      <div>
-        <DayPilotCalendar
-          {...config}
-          onTimeRangeSelected={args => {
-            this.calendar.message("Selected range: " + args.start.toString("hh:mm tt") + " - " + args.end.toString("hh:mm tt"));
-          }}
-          ref={this.calendarRef}
-        />
+      <div> <h1>
+        Hello, welcome to your week.
+        </h1>
+        <div style={styles.wrap}>
+        <div style={styles.left}>
+        </div>
+        <div style={styles.main}>
+          <DayPilotCalendar
+            {...this.state}
+            ref={this.calendarRef}
+          />
+        </div>
+        <div style={styles.wrap}>
+           <button onClick={this.createItem} className="btn btn-info">
+        Add task
+        </button>
+       
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ) : null}
+        </div>
       </div>
+        </div>
+      
+      
+      
     );
+    
   }
 }
 
